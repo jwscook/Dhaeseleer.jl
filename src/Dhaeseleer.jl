3277e31734b857cafde528f6236e34b3ce124cb8
James Cook <cookjws@gmail.com>
2024-12-14 18:42:42 +0000

    this doesnt work but ive got an idea

--- src/Dhaeseleer.jl
+++ src/Dhaeseleer.jl
@@ -2,6 +2,15 @@ module Dhaeseleer
 
 using Symbolics, LinearAlgebra
 
+function process(a)
+  #return Symbolics.expand_derivatives(a)
+# a = Symbolics.expand_derivatives(a, true)
+# return Symbolics.simplify(a)
+#  a = Symbolics.expand_derivatives(Symbolics.simplify(a))
+  return a
+end
+
+
 abstract type AbstractCoordinateSystem end
 
 struct ArbitraryCoordinateSystem <: AbstractCoordinateSystem
@@ -33,6 +42,7 @@ function CoordinateSystem(xi, ui)
   ∂u⃗_∂x⃗ = [∂x(ui[1]) ∂y(ui[1]) ∂z(ui[1]);
            ∂x(ui[2]) ∂y(ui[2]) ∂z(ui[2]);
            ∂x(ui[3]) ∂y(ui[3]) ∂z(ui[3])]
+  ∂u⃗_∂x⃗ .= process.(∂u⃗_∂x⃗)
   c = ArbitraryCoordinateSystem(xi, ui, ∂₁, ∂₂, ∂₃, ∂u⃗_∂x⃗)
   return (∇(c), ∇o(c), ∇x(c))
 end
@@ -49,7 +59,7 @@ for (CV, Ai) in ((:CovariantVector, :Aᵢ), (:ContravariantVector, :Aⁱ))
     cs::C
     $(Ai)::Vector{Num} # for a component Aⁱ uᵢ, this represents Aⁱ
     function $(CV)(cs::C, Ai::Vector{Num}) where {C<:AbstractCoordinateSystem}
-      return new{C}(cs, Symbolics.expand_derivatives(Ai))
+      return new{C}(cs, process.(Ai))
     end
   end
 end
@@ -63,38 +73,43 @@ Base.setindex!(a::CovariantVector, v, i) = (a.Aⁱ[i] = v)
 Base.getindex(a::ContravariantVector, i) = a.Aᵢ[i]
 Base.setindex!(a::ContravariantVector, v, i) = (a.Aᵢ[i] = v)
 
-function Base.convert(::Type{ContravariantVector}, a::CovariantVector)
+function Base.convert(a::CovariantVector)
   cs = coordinatesystem(a)
   return ContravariantVector(cs, gⁱʲ(cs) * a.Aᵢ)
 end
 
-function Base.convert(::Type{CovariantVector}, a::ContravariantVector)
+function Base.convert(a::ContravariantVector)
   cs = coordinatesystem(a)
   return CovariantVector(cs, gᵢⱼ(cs) * a.Aⁱ)
 end
 
-Base.convert(::Type{ContravariantVector}) = CovariantVector
-Base.convert(::Type{CovariantVector}) = ContravariantVector
+#Base.convert(::Type{ContravariantVector}) = CovariantVector
+#Base.convert(::Type{CovariantVector}) = ContravariantVector
 
-function Base.:*(a::Num, b::ContravariantVector)
-  return ContravariantVector(coordinatesystem(b), a .* b.Aⁱ)
-end
+#function Base.:*(a::Num, b::ContravariantVector)
+#  return ContravariantVector(coordinatesystem(b), a .* b.Aⁱ)
+#end
 Base.:*(a::AbstractCoconVector, b::Num) = b * a
 for (CV, Ai) in ((CovariantVector, :Aᵢ), (ContravariantVector, :Aⁱ))
   for op in (:+, :-)
     @eval function Base.$(op)(a::T, b::T) where {T<:$(CV)}
       cs = coordinatesystem(a)
       @assert cs == coordinatesystem(b)
-      return CovariantVector(cs, a.$(Ai) + b.$(Ai))
+      return CovariantVector(cs, $(op)(a.$(Ai), b.$(Ai)))
     end
   end
   for op in (:*, :/)
     @eval function Base.$(op)(a::$(CV), b::Num)
-      return CovariantVector(coordinatesystem(b), op.(a.$(Ai), b))
+      return CovariantVector(coordinatesystem(a), $(op).(a.$(Ai), b))
     end
   end
+  @eval Base.:-(a::$(CV)) = CovariantVector(coordinatesystem(a), -a.$(Ai))
 end
 Base.:*(a::Num, b::AbstractCoconVector) = b * a
+Base.:+(a::CovariantVector, b::ContravariantVector) = convert(a) + b
+Base.:+(a::ContravariantVector, b::CovariantVector) = convert(a) + b
+Base.:-(a::CovariantVector, b::ContravariantVector) = convert(a) - b
+Base.:-(a::ContravariantVector, b::CovariantVector) = convert(a) - b
 
 coordinatesystem(a) = a.cs
 gᵢⱼ(c::AbstractCoordinateSystem) = c.∂u⃗_∂x⃗ * c.∂u⃗_∂x⃗'
@@ -109,7 +124,7 @@ function (d::∇o)(a::ContravariantVector)
   cs = coordinatesystem(a)
   J = 𝐽(cs)
   f = (cs.∂₁(J * a.Aⁱ[1]) + cs.∂₂(J * a.Aⁱ[2]) + cs.∂₃(J * a.Aⁱ[3])) / J
-  return Symbolics.expand_derivatives(f)
+  return process(f)
 end
 ∇o(a::CovariantVector) = ∇o(convert(a))
 
@@ -120,21 +135,31 @@ function (d::∇x)(a::CovariantVector)
                                   cs.∂₃(a.Aᵢ[1]) - cs.∂₁(a.Aᵢ[3]) / J,
                                   cs.∂₁(a.Aᵢ[2]) - cs.∂₂(a.Aᵢ[1]) / J])
 end
-∇x(a::ContravariantVector) = ∇x(convert(CovariantVector, a))
+∇x(a::ContravariantVector) = ∇x(convert(a))
 
 import LinearAlgebra: cross, dot
-dot(a::ContravariantVector, b::CovariantVector) = dot(a.Aⁱ, b.Aᵢ)
+function dot(a::ContravariantVector, b::CovariantVector)
+  f = dot(a.Aⁱ, b.Aᵢ)
+  return process(f)
+end
 dot(a::CovariantVector, b::ContravariantVector) = dot(b, a)
-dot(a::T, b::T) where {T<:AbstractCoconVector} = dot(a, convert(convert(T), b))
+dot(a::T, b::T) where {T<:AbstractCoconVector} = dot(a, convert(b))
 function cross(a::CovariantVector, b::CovariantVector)
   cs = coordinatesystem(a)
-  return ContravariantVector(cs, cross(a, b) ./ 𝐽(cs))
+  return ContravariantVector(cs, cross(a.Aᵢ, b.Aᵢ) ./ 𝐽(cs))
 end
 function cross(a::ContravariantVector, b::ContravariantVector)
   cs = coordinatesystem(a)
   return ContravariantVector(cs, cross(a.Aⁱ, b.Aⁱ) .* 𝐽(cs))
 end
-cross(a::CovariantVector, b::ContravariantVector) = cross(a, convert(CovariantVector, b))
-cross(a::ContravariantVector, b::CovariantVector) = cross(convert(CovariantVector, a), b)
+cross(a::CovariantVector, b::ContravariantVector) = cross(convert(a), b)
+cross(a::ContravariantVector, b::CovariantVector) = -cross(b, a)
 export cross, dot
+
+import Base: abs2, abs
+# aᵢ uⁱ = a_ψ ∇ψ + a_θ ∇θ + a_φ ∇φ
+abs2(a::ContravariantVector) = (a.Aⁱ' * a.Aⁱ)
+abs2(a::CovariantVector) = (a.Aᵢ' * a.Aᵢ)
+abs(a::AbstractCoconVector) = sqrt(abs2(a))
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,18 +3,24 @@ using Test
 
 @testset "Dhaeseleer.jl" begin
   @variables x, y, z
-  R = sqrt(x^2 + y^2)
-  ϕ = atan(y, x)
-  Z = z
+  #R = sqrt(x^2 + y^2)
+  #ϕ = atan(y, x)
+  #Z = z
+  @variables R(x, y, z) ϕ(x, y, z) Z(x, y, z)
   ∇, ∇o, ∇x = Dhaeseleer.CoordinateSystem([x, y, z], [R, ϕ, Z])
   @variables B₀ R₀ κ₀ q₀
 
   Ψ = B₀ / (2 * R₀^2 * κ₀ * q₀) * (R * Z + κ₀^2 / 4 * (R^2 - R₀^2))
   ∇Ψ = ∇(Ψ)
   ∇ϕ = ∇(ϕ)
+  #@show Symbolics.substitute(∇ϕ, [sqrt(x^2 + y^2)=>R, atan(y, x)=>ϕ, z=>Z])
   fΨ = B₀ * R₀
   B = fΨ * ∇ϕ + cross(∇Ψ, ∇ϕ)
   J = ∇x(B)
-  @test ∇o(J) == 0
+  ∇oJ = ∇o(J)
+  ∇oJ = Symbolics.substitute(∇oJ, [sqrt(x^2 + y^2)=>R, atan(y, x)=>ϕ, z=>Z])
+  @test ∇oJ == 0
+  T = cross(∇Ψ, B) / abs(B)
+  @test dot(T, ∇Ψ) == 0
 
 end
